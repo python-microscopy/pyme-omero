7f9e8ad655e6ca5ef2278a57f9895dc66101c2c0
barentine <andrew.barentine@gmail.com>
2020-08-26 09:14:37 -0400

    add project linking

--- pyme_omero/core.py
+++ pyme_omero/core.py
@@ -17,47 +17,64 @@ with open(credentials) as f:
 LOGIN_ARGS = ['-u%s' % credentials['user'], '-w%s' % credentials['password'], 
               '-s%s' % credentials['address'], '-p%s' % credentials.get('port', 4064)]
 
-def create_dataset(name):
-    with cli_login(*LOGIN_ARGS) as cli:
-        conn = BlitzGateway(client_obj=cli._client)
-        dataset = omero.model.DatasetI()
-        dataset.setName(rtypes.rstring(name))
-        dataset = conn.getUpdateService().saveAndReturnObject(dataset)
-        return dataset.getId().getValue()
+def create_dataset(connection, dataset_name):
+    dataset = omero.model.DatasetI()
+    dataset.setName(rtypes.rstring(dataset_name))
+    dataset = connection.getUpdateService().saveAndReturnObject(dataset)
+    return dataset.getId().getValue()
 
-def get_or_create_project_id(project_name):
-    with cli_login(*LOGIN_ARGS) as cli:
-        conn = BlitzGateway(client_obj=cli._client)
-        
-        # check if the project already exists
-        projects = conn.getContainerService().loadContainerHierarchy('Project', 
-                                                                     None, None)
-        for p in projects:
-            if p.getName().getValue() == project_name:
-                return p.getId().getValue()
-        
-        # create a new project
-        project = omero.model.ProjectI()
-        project.setName(rtypes.rstring(project_name))
-        project = conn.getUpdateService().saveAndReturnObject(project)
-        return project.getId().getValue()
+def get_or_create_project_id(connection, project_name):    
+    # check if the project already exists
+    projects = connection.getContainerService().loadContainerHierarchy('Project', 
+                                                                    None, None)
+    for p in projects:
+        if p.getName().getValue() == project_name:
+            return p.getId().getValue()
+    
+    # create a new project
+    project = omero.model.ProjectI()
+    project.setName(rtypes.rstring(project_name))
+    project = connection.getUpdateService().saveAndReturnObject(project)
+    return project.getId().getValue()
 
-def get_or_create_dataset_id(dataset_name):
+def get_or_create_dataset_id(dataset_name, project_name=None):
     with cli_login(*LOGIN_ARGS) as cli:
         conn = BlitzGateway(client_obj=cli._client)
         
-        # check if the dataset already exists
+        # handle linking with project if given
+        if project_name != None:
+            # check if the dataset already exists within the project
+            project_id = get_or_create_project_id(conn, project_name)
+            # projects = conn.getContainerService().loadContainerHierarchy('Project', 
+                                                                       #  None, None)
+            project = conn.getObject("Project", project_id)
+            dataset_wrapper = project.findChildByName(dataset_name)
+            if dataset_wrapper != None:
+                return dataset_wrapper.getId()
+            
+            # project._toggleCollectionsLoaded(True)
+            #linked_datasets = project.linkedDatasetList()
+
+            # FIXME - add flag to check if dataset already exists but is unlinked?
+            dataset_id = create_dataset(conn, dataset_name)
+            # link dataset to project
+            links = []
+            link = omero.model.ProjectDatasetLinkI()
+            link.parent = omero.model.ProjectI(project_id, False)
+            link.child = omero.model.DatasetI(dataset_id, False)
+            links.append(link)
+            conn.getUpdateService().saveArray(links, conn.SERVICE_OPTS)
+            return dataset_id
+        
+        # no project specified, check if the dataset already exists
         datasets = conn.getContainerService().loadContainerHierarchy('Dataset', 
-                                                                     None, None)
+                                                                    None, None)
         for d in datasets:
             if d.getName().getValue() == dataset_name:
                 return d.getId().getValue()
         
-        # create a new dataset
-        dataset = omero.model.DatasetI()
-        dataset.setName(rtypes.rstring(dataset_name))
-        dataset = conn.getUpdateService().saveAndReturnObject(dataset)
-        return dataset.getId().getValue()
+        # otherwise create a new dataset
+        return create_dataset(conn, dataset_name)
 
 def file_import(client, file, wait=-1):
     """Re-usable method for a basic import."""
@@ -74,10 +91,11 @@ def file_import(client, file, wait=-1):
     finally:
         proc.close()
 
-def upload_image_from_file(file, dataset_name, attachments=(), wait=-1):
+def upload_image_from_file(file, dataset_name, project_name=None, 
+                           attachments=(), wait=-1):
     attachments = list(attachments)
 
-    dataset_id = get_or_create_dataset_id(dataset_name)
+    dataset_id = get_or_create_dataset_id(dataset_name, project_name)
 
     with cli_login() as cli:
         conn = BlitzGateway(client_obj=cli._client)
@@ -110,5 +128,5 @@ def upload_image_from_file(file, dataset_name, attachments=(), wait=-1):
 
 if __name__ == '__main__':
     upload_image_from_file('/Users/Andrew/Desktop/112418ROI00877.png',
-                           'pyme_omero_testing1', 
+                           'pyme_omero_testing2', project_name='andrew-testing',
                            attachments=['/Users/Andrew/Desktop/112418ROI00877.hdf'])
