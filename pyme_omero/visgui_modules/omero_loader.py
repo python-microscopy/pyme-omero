b5baa0c3f6ca62f862630c2d0b3157f5d9550def
Barentine <andrew.barentine@yale.edu>
2020-09-10 13:13:36 -0400

    add functionality to add or run omero output modules from the gui

--- pyme_omero/visgui_modules/omero_loader.py
+++ pyme_omero/visgui_modules/omero_loader.py
@@ -13,6 +13,7 @@ class OMEROLoader(object):
         logging.debug('Adding menu items for OMERO loading')
 
         vis_frame.AddMenuItem('File', 'Open OMERO', self.OnOpenOMERO)
+        vis_frame.AddMenuItem('File', 'Save to OMERO', self.OnSaveToOMERO)
 
     def OnOpenOMERO(self, wx_event=None):
         from pyme_omero.core import localization_files_from_image_url
@@ -35,8 +36,26 @@ class OMEROLoader(object):
         
         self.vis_frame.SetFit()
     
+    def OnSaveToOMERO(self, wx_event=None):
+        from pyme_omero.recipe_modules import omero_upload
+        import os
+
+        context = dict(file_stub=os.path.splitext(os.path.split(self.pipeline.filename)[-1])[0])
+
+        omero_mods = [mod for mod in self.pipeline.recipe.modules if isinstance(mod, omero_upload.RGBImageUpload)]
+        
+        if len(omero_mods) == 0:
+            # we don't have any omero output modules, make one
+            uploader = omero_upload.RGBImageUpload(self.pipeline.recipe)
+            if uploader.configure_traits(kind='modal'):
+                omero_mods.append(uploader)
+
+        for mod in omero_mods:
+            mod.save(self.pipeline.recipe.namespace, context)
+    
     def __del__(self):
         self._tempdir.cleanup()
+
 
 def Plug(vis_frame):
     vis_frame.omero_loader = OMEROLoader(vis_frame)
