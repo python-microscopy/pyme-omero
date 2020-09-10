54f8dd280daf8692b70e8d4ea96f72910928912a
barentine <andrew.barentine@gmail.com>
2020-09-10 19:31:22 -0400

    run install plugin script automatically after develop/install

--- setup.py
+++ setup.py
@@ -1,10 +1,40 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+
+def install_pyme_plugin():
+    import sys
+    import subprocess
+    import os
+    plugin_install_path = os.path.join(os.path.dirname(__file__), 
+                                           'pyme_omero', 'install_plugin.py')
+    subprocess.Popen('%s %s' % (sys.executable, plugin_install_path), 
+                        shell=True)
+
+
+class DevelopModuleAndInstallPlugin(develop):
+    """Post-installation for development mode."""
+    def run(self):
+        develop.run(self)
+        install_pyme_plugin()
+        
+
+class InstallModuleAndInstallPlugin(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        install.run(self)
+        install_pyme_plugin()
+
 
 setup(
     name='pyme_omero',
-    version='20.08.25',
+    version='20.09.10',
     description='pyme-omero interoperability',
     packages=find_packages(),
+    cmdclass={
+        'develop': DevelopModuleAndInstallPlugin,
+        'install': InstallModuleAndInstallPlugin,
+    },
 )
